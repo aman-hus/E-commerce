f67bfee49a3d19a24cf9c03b915cccebce0b7c6c
aman hussain <aman30dbg@gmail.com>
2025-05-16 01:31:48 +0530

    product page updated

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,9 +1,21 @@
-import React from 'react' 
+import React, { useContext } from 'react' 
+import { ShopContext } from '../context/ShopContext'
+import { useParams } from 'react-router-dom';
+import Breadcrum from '../component/Breadcrums/Breadcrum'
+import ProductDisplay from '../component/ProductDisplay/ProductDisplay'
 
 const Product = () => {
+    const {all_product}=useContext(ShopContext);
+    const {productId}=useParams();
+    const product = all_product.find((e)=>e.id === Number(productId));
+    console.log("Product:", product);
+
+
     return (
         <div>
-
+            <Breadcrum product={product}/>
+            <ProductDisplay product={product} />
+            
         </div>
     )
 }
