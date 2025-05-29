91a46f4469e08dc652bd013d30d3befb12cde28a
aman hussain <aman30dbg@gmail.com>
2025-05-30 00:38:10 +0530

    frontend ready

--- src/component/CartItems/CartItems.jsx
+++ src/component/CartItems/CartItems.jsx
@@ -22,7 +22,7 @@ const CartItems=()=>{
               <div className="cartitems-format cartitems-format-main">
                 <img src={e.image} alt="" className='carticon-product-icon' />
                 <p>{e.name}</p>
-                <p>{e.new_price}</p>
+                <p>${e.new_price}</p>
                 <button className='cartitems-quantity'>{cartItems[e.id]}</button>
                 <p>${e.new_price*cartItems[e.id]}</p>
                 <img clasname='cartitems-remove-icon'src={remove_icon} onClick={()=>{removeFromCart(e.id)}} alt="" />
